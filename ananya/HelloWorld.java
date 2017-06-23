8edae6a12becc3ac2577a113704cf7c1d6638cd7
ananya-technicise <ananyasinha5@gmail.com>
2017-06-23 16:41:15 +0530

    Hello World Java Programming

--- ananya/HelloWorld.java
+++ ananya/HelloWorld.java
@@ -1,10 +1,7 @@
-/*http://ideone.com/9jpaU4
-* just print helloworld
-*/
 class HelloWorld
 {
-  public static void main(String args [])
-  {
-    System.out.println("Hello, World!");
-  }
+		public static void main(String args[])
+		{
+		System.out.println("Hello World");
+		}
 }
