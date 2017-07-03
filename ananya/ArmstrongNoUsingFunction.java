463287efe9ef0a6d469c962772aa5e1873e2b63c
ananya-technicise <ananyasinha5@gmail.com>
2017-07-03 17:04:19 +0530

    Armstrong No using function

---
+++ ananya/ArmstrongNoUsingFunction.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ArmstrongNoUsingFunction
+{
+	public static void main(String args[])
+	{
+		PrintArmstrongNo();
+	}
+		public static void PrintArmstrongNo()
+			{
+				System.out.println("Enter the no which you want check armstrong number or not: ");
+				Scanner scnr = new Scanner(System.in);
+				int n = scnr.nextInt();
+				int temp, a, c = 0;
+				temp = n;
+				while(n > 0)
+				{
+					a = n % 10;
+					n = n / 10;
+					c = c + (a * a * a);
+				}
+					if(temp == c)
+					System.out.println("This no is armstrong no.");
+					else
+					System.out.println("This is not armstrong no.");
+			}
+}
