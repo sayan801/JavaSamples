aecd3cc643387ce336c1614b22ade39fed4d647c
suman-technicise <dustu9944it@gmail.com>
2017-07-07 13:12:15 +0530

    deletion of 3 char in the middle of a any word

--- ananya/Deletion.java
+++ ananya/Deletion.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 public class Deletion
 {
-	public static void main(String args[])
-	{
-		Scanner scnr = new Scanner(System.in);
-		
-		System.out.println("Enter a word:");
-		String word = scnr.nextLine();
-		
-		int l =  word.length();
-		int odd = (l+1)/2;
-		int even = l/2;
-		if(l % 2 == 0)
-		{			
-			StringBuffer sb = new StringBuffer(word);
-			sb.delete(even - 2, even + 2);
-			System.out.println(sb);
-		}
-		else
-			{
-				StringBuffer sb = new StringBuffer(word);
-				sb.delete(odd - 2, odd + 2);
-				System.out.println(sb);
-			
-			}
-			
-		
-	}
+    public static void main(String args[])
+    {
+        Scanner scnr = new Scanner(System.in);
+        
+        System.out.println("Enter a word:");
+        String word = scnr.nextLine();
+        
+        int l =  word.length();
+        int odd = (l+1)/2;
+        int even = l/2;
+        if(l % 2 == 0)
+        {            
+            StringBuffer sb = new StringBuffer(word);
+            sb.delete(even - 2, even + 1);
+            System.out.println(sb);
+        }
+        else
+            {
+                StringBuffer sb = new StringBuffer(word);
+                sb.delete(odd - 2, odd + 1);
+                System.out.println(sb);
+            
+            }
+            
+        
+    }
 }
