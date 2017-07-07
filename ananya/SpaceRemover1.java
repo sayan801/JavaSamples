a3fd7001e7bbb73c36ce2e91494ee5323fd0e88a
suman-technicise <dustu9944it@gmail.com>
2017-07-07 13:13:10 +0530

    remove space and concate  a string

---
+++ ananya/SpaceRemover1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SpaceRemover1
+{
+	public static void main(String args[])
+	{
+		
+		String S1 = "I am Ananya";
+		
+		String S2 = S1.replaceAll(" " , "");
+		System.out.println(S2);	
+	}
+}
