d7b45119234a19b8a5e9c1fae4b9acd1840ef447
suman-technicise <dustu9944it@gmail.com>
2017-07-07 13:08:24 +0530

    two string are same or not using compareto()

--- ananya/CompareString.java
+++ ananya/CompareString.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 public class CompareString
 {
-	public static void main(String args[])
-	{
-		Scanner scnr = new Scanner(System.in);
-		
-		System.out.println("Enter String1:");
-		String S1 = scnr.nextLine();
-		S1 = S1.toLowerCase();
-		
-		System.out.println("Enter String2:");
-		String S2 = scnr.nextLine();
-		S2 = S2.toLowerCase();
-		
-		if(S1.compareTo(S2) == 0)
-			System.out.println("String1 is equal to String2.");
-		
-		else 
-			System.out.println("String1 is not equal to String2.");
-		
-	}
+    public static void main(String args[])
+    {
+        Scanner scnr = new Scanner(System.in);
+        
+        System.out.println("Enter String1:");
+        String S1 = scnr.nextLine();
+        
+        System.out.println("Enter String2:");
+        String S2 = scnr.nextLine();
+        
+        if(S1.compareTo(S2) == 0)
+            System.out.println("String1 is equal to String2.");
+        
+        else 
+            System.out.println("String1 is not equal to String3.");
+        
+    }
 }
