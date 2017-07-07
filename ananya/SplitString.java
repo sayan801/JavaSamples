03c9078c1d1a84097d5d9d289ed9e965c5eeaadd
suman-technicise <dustu9944it@gmail.com>
2017-07-07 13:14:41 +0530

    split a word from a given position

---
+++ ananya/SplitString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SplitString
+{
+
+	public static void main(String args[])
+	{
+		String strg1 = "AnanyaTechniciseSinha";
+		
+		System.out.println(strg1.substring(0,6));	
+		System.out.println(strg1.substring(6,16));
+		System.out.println(strg1.substring(16,21));  	
+	}
+}
