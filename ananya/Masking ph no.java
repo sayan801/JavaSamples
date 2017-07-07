669677aea664a01f0c617b10e21788f1b2bde306
ananya-technicise <ananyasinha5@gmail.com>
2017-07-07 08:14:59 +0000

    Mask a phone no first 4 character

---
+++ ananya/Masking ph no.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Masking
+{
+	public static void main(String args[])
+	{
+		String PhoneNo = "9333112611";
+
+		String Strg1 = "****";
+		String Strg2 = PhoneNo.substring(4);
+
+		string1 = strg1 + " ";
+		Strg1 = Strg1.concat(Strg2);
+
+		System.out.println("After masking: " + Strg1);
+		
+	}
+}
