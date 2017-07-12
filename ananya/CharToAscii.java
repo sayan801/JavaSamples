8ec06870ddb38710d2bfcf468243c878edd31a59
ananya-technicise <ananyasinha5@gmail.com>
2017-07-12 18:20:46 +0530

    Convert Alphabetical character to ascii

---
+++ ananya/CharToAscii.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CharToAscii
+{
+public static void main(String args[])
+	{
+		char a = 'A';
+		int x = (int)a;
+		
+		System.out.println("CharToAscii  of caps 'A' is     " + x);
+	}
+}
