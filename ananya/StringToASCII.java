4854b46d4a42b1ae72d96b0862cb1e19654ceb7e
ananya-technicise <ananyasinha5@gmail.com>
2017-07-14 16:01:36 +0530

    Convert alphabet into their coresponding Ascii value

---
+++ ananya/StringToASCII.java
@@ -0,0 +1,20 @@
+public class StringToASCII
+{
+
+	public static void main(String args[])
+	{
+        
+
+        char A = 'A';
+        int ascii = A;
+        System.out.println("ASCII value of 'A' is  : " + ascii);
+        
+
+        char a = 'a';
+        int value = (int) a;
+        System.out.println("ASCII value of 'a' is  : " + value);
+        
+	}
+}
+
+
